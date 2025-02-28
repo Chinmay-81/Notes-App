b1ba27a234834c19fd85010961bbeba722127aaa
Chinmay Nayak <nchinmay66@gmai.com>
2025-02-28 19:18:14 +0530

    Notes-App Commit

---
+++ src/main/java/com/nayak/Notes_APP/NotesAppApplication.java
@@ -0,0 +1,14 @@
+package com.nayak.Notes_APP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(NotesAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nayak/Notes_APP/repo/NotesRepo.java
@@ -0,0 +1,10 @@
+package com.nayak.Notes_APP.repo;
+
+import com.nayak.Notes_APP.entity.Notes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotesRepo extends JpaRepository<Notes,Integer> {
+    List<Notes> findByLanguage(String language);
+}
